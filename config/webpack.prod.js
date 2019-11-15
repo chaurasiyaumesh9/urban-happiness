c59bb6814f8e8b63f5b4f5157963a9b2d880a650
umesh c <chaurasiyaumesh9@gmail.com>
2019-11-15 17:29:15 +0530

    change in webpack config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,32 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
 
-const helpers = require('./helpers');
-const commonConfig = require('./webpack.common');
+const helpers = require("./helpers");
+const commonConfig = require("./webpack.common");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(commonConfig, {
-  mode: 'production',
+  mode: "production",
 
   output: {
-    filename: 'js/[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
+    filename: "js/[name].[hash].js",
+    chunkFilename: "[id].[hash].chunk.js"
   },
-
-  plugins: [
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  }
+  /*plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false,
@@ -22,5 +36,5 @@ module.exports = merge(commonConfig, {
         comments: false
       }
     })
-  ]
+  ]*/
 });
