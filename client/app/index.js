9229cd54c9ef939eca65d545ccd8f93f8700a288
umesh c <chaurasiyaumesh9@gmail.com>
2019-11-26 19:49:27 +0530

    UI changes

--- client/app/index.js
+++ client/app/index.js
@@ -1,9 +1,9 @@
-// import "bootstrap";
+import "bootstrap";
 // import "@fortawesome/fontawesome-free/js/fontawesome";
 // import "@fortawesome/fontawesome-free/js/solid";
 // import "@fortawesome/fontawesome-free/js/regular";
 // import "@fortawesome/fontawesome-free/js/brands";
-import "bootstrap/dist/css/bootstrap.min";
+// import "bootstrap/dist/css/bootstrap.min";
 
 import React from "react";
 import ReactDOM from "react-dom";
